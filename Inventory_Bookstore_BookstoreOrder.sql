6eb64806cf35128c953bcccee99f03165a4721fb
th3bigch33s3 <47068404+th3bigch33s3@users.noreply.github.com>
2020-07-19 16:04:48 -0400

    Create Inventory_Bookstore_BookstoreOrder.sql
    sql for the bookstore, inventory and bookstore orders

---
+++ Inventory_Bookstore_BookstoreOrder.sql
@@ -0,0 +1,97 @@
+
+INSERT INTO `eyc353_1`.`Bookstore_Publisher_Order` (`Publishers_publisher_number`, `Bookstore_bookstore_id`, `order_id`, `confirmation`) VALUES ('1', '11', '1', '0');
+INSERT INTO `eyc353_1`.`Bookstore_Publisher_Order` (`Publishers_publisher_number`, `Bookstore_bookstore_id`, `order_id`, `confirmation`) VALUES ('1', '16', '6', '0');
+
+INSERT INTO `eyc353_1`.`Bookstore_Publisher_Order` (`Publishers_publisher_number`, `Bookstore_bookstore_id`, `order_id`, `confirmation`) VALUES ('1', '19', '2', '1');
+INSERT INTO `eyc353_1`.`Bookstore_Publisher_Order` (`Publishers_publisher_number`, `Bookstore_bookstore_id`, `order_id`, `confirmation`) VALUES ('3', '11', '3', '0');
+
+INSERT INTO `eyc353_1`.`Bookstore_Publisher_Order` (`Publishers_publisher_number`, `Bookstore_bookstore_id`, `order_id`, `confirmation`) VALUES ('4', '10', '5', '1');
+INSERT INTO `eyc353_1`.`Bookstore_Publisher_Order` (`Publishers_publisher_number`, `Bookstore_bookstore_id`, `order_id`, `confirmation`) VALUES ('4', '13', '7', '1');
+
+INSERT INTO `eyc353_1`.`Bookstore_Publisher_Order` (`Publishers_publisher_number`, `Bookstore_bookstore_id`, `order_id`, `confirmation`) VALUES ('5', '16', '8', '1');
+INSERT INTO `eyc353_1`.`Bookstore_Publisher_Order` (`Publishers_publisher_number`, `Bookstore_bookstore_id`, `order_id`, `confirmation`) VALUES ('9', '15', '4', '0');
+
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('1', '11', '0-2237-3192-7', '1', '2020-01-02 00:00:00', '1');
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('2', '12', '0-2710-5457-3', '1', '2020-01-03 00:00:00', '1'
+);
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('3', '13', '0-2838-9190-4', '1', '2020-01-04 00:00:00', '0'
+);
+
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('4', '14', '0-3432-6145-6', '1', '2020-01-05 00:00:00', '0'
+);
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('5', '15', '0-4193-9813-9', '1', '2020-01-06 00:00:00', '1'
+
+);
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('6', '16', '0-4809-1234-3', '1', '2020-01-07 00:00:00', '0'
+);
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('7', '17', '0-4838-6782-9', '1', '2020-01-08 00:00:00', '1'
+);
+
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('8', '18', '0-6034-3910-1', '1', '2020-01-09 00:00:00', '1'
+);
+
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('9', '19', '0-6521-1546-2', '2', '2020-01-10 00:00:00', '1'
+);
+
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('10', '10', '0-1188-0050-7', '1', '2020-01-01 00:00:00', '0'
+);
+
+
+INSERT INTO `eyc353_1`.`Inventory` (`inventory_id`, `bookstore_id`, `ISBN`, `quantity`, 'day_received', 'is_reserved') VALUES ('11', '10', '0-2838-9190-4', '3', '2020-07-13 08:12:00', '1'
+);
+
+
+
+
+
+INSERT INTO `eyc353_1`.`Bookstore` (`bookstore_id`, `name`, `telephone`, `email_address`, 'address', 'postal_code', 'president') VALUES ('10', 'a', '11111111', 'a@hotmail.com', '123 street', 'a11222', 'Mr.Burns'
+);
+
+
+INSERT INTO `eyc353_1`.`Bookstore` (`bookstore_id`, `name`, `telephone`, `email_address`, 'address', 'postal_code', 'president') VALUES ('11', 'b', '2222222', 'b@hotmail.com', '222 street', 'b11222', 'Peter'
+);
+
+
+INSERT INTO `eyc353_1`.`Bookstore` (`bookstore_id`, `name`, `telephone`, `email_address`, 'address', 'postal_code', 'president') VALUES ('12', 'c', '3333333', 'c@hotmail.com', '333 street', 'c11222', 'Yugi Motto'
+);
+
+INSERT INTO `eyc353_1`.`Bookstore` (`bookstore_id`, `name`, `telephone`, `email_address`, 'address', 'postal_code', 'president') VALUES ('13', 'd', '4444444', 'd@hotmail.com', '444 street', 'd11222', 'Evan'
+);
+
+
+INSERT INTO `eyc353_1`.`Bookstore` (`bookstore_id`, `name`, `telephone`, `email_address`, 'address', 'postal_code', 'president') VALUES ('14', 'e', '5555555', 'e@hotmail.com', '555 street', 'e11222', 'Goku'
+);
+
+
+INSERT INTO `eyc353_1`.`Bookstore` (`bookstore_id`, `name`, `telephone`, `email_address`, 'address', 'postal_code', 'president') VALUES ('15', 'f', '6666666', 'f@hotmail.com', '666 street', 'f11222', 'Kien'
+
+);
+
+INSERT INTO `eyc353_1`.`Bookstore` (`bookstore_id`, `name`, `telephone`, `email_address`, 'address', 'postal_code', 'president') VALUES ('16', 'g', '7777777', 'g@hotmail.com', '777 street', 'g11222', 'Kasra'
+);
+
+INSERT INTO `eyc353_1`.`Bookstore` (`bookstore_id`, `name`, `telephone`, `email_address`, 'address', 'postal_code', 'president') VALUES ('17', 'h', '8888888', 'h@hotmail.com', '888 street', 'h11222', 'Pierre'
+);
+
+INSERT INTO `eyc353_1`.`Bookstore` (`bookstore_id`, `name`, `telephone`, `email_address`, 'address', 'postal_code', 'president') VALUES ('18', 'i', '9999999', 'i@hotmail.com', '999 street', 'i11222', 'Kobe'
+);
+
+INSERT INTO `eyc353_1`.`Bookstore` (`bookstore_id`, `name`, `telephone`, `email_address`, 'address', 'postal_code', 'president') VALUES ('19', 'j', '0', 'j@hotmail.com', '000 street', 'j11222', 'Brenden'
+);
+
+
+
+
+
+
+
