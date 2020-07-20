71eef260a4f30b66f9f3f74dcefe916789afdbb1
PWatine <35372462+PWatine@users.noreply.github.com>
2020-07-19 23:03:59 -0400

    Question 5 query

---
+++ question5.sql
@@ -0,0 +1,7 @@
+
+SELECT COUNT(*) FROM arrived_books;
+SELECT COUNT(*) FROM Book_Author;
+SELECT COUNT(*) FROM Bookstore_Publisher_Order;
+SELECT COUNT(*) FROM Orders;
+SELECT COUNT(*) FROM Reader_Interest;
+SELECT COUNT(*) FROM Special_Order;
